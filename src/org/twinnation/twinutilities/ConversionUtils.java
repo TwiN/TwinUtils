0c8ba95a9434ba14c6509d0414769e05883e22c7
TwinProduction <christian-ct@hotmail.com>
2016-12-03 17:19:03 -0500

    Updated ConversionUtils

--- src/org/twinnation/twinutilities/ConversionUtils.java
+++ src/org/twinnation/twinutilities/ConversionUtils.java
@@ -13,56 +13,82 @@ public final class ConversionUtils {
 	
 	private ConversionUtils() {}
 	
+	
 	/**
-	 * Methode qui transforme une liste de char en String
-	 * @return str
+	 * Converts a char array to a String
+	 * @return String 
 	 */
 	public static String chars2String(char[] c) {
-		String str = "";
+		String result = "";
 		int x = 0;
-		while (x <= c.length-1) {
-			str += ""+c[x++];
-		}
-		return str;
+		while (x <= c.length-1) { result += ""+c[x++]; }
+		return result;
 	}
 	
 	
 	/**
-	 * Methode qui converti une chaine de caractere en element de type Document.
-	 * @param contents Chaine de caractere a convertir
-	 * @return document normalisee
+	 * Converts a string to a Document
+	 * @param contents String to convert
+	 * @return Document object containing contents 
 	 */
 	public static Document string2Document(String contents) {
 		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
 		DocumentBuilder builder;
-		Document document = null;
+		Document result = null;
 		try {
 			builder = factory.newDocumentBuilder();
-			document = builder.parse(new InputSource(new StringReader(contents)));
-			document.getDocumentElement().normalize();
+			result = builder.parse(new InputSource(new StringReader(contents)));
+			result.getDocumentElement().normalize();
 		} catch (Exception e) {
 			System.out.println("Unable to convert string to Document: "
 					+ e.getMessage());
 		}
-		return document;
+		return result;
 	}
 	
 	
 	/**
-	 * Methode qui converti une liste en String avec un separateur entre chaque
-	 * element de la liste.
-	 * @param separator Separateur
-	 * @param array Liste
-	 * @return String avec un separateur entre chaque element de la liste
+	 * Converts a String array into a string separated by the separator passed
+	 * as parameter
+	 * @param separator String to put between every element of the array
+	 * @param array List to convert
+	 * @return String of the array with a separator between each elements
 	 */
 	public static String implode(String separator, String[] array) {
-		if (array == null) {
-			return "";
-		}
-		String str = "";
+		if (array == null) { return ""; }
+		String result = "";
 		for (int i = 0; i<array.length; i++) {
-			str += array[i]+""+(array.length-1==i?"":separator);
+			result += array[i]+""+(array.length-1==i?"":separator);
 		}
-		return str;
+		return result;
+	}
+	
+	
+	/**
+	 * Pads a number with 0 until it has the expected amount of digits. 
+	 * If the numToPad has more than the amount of digit expected, 
+	 * no change will be made to the String.
+	 * @param digitExpected Number of digits expected
+	 * @param numToPad Number to pad with 0s
+	 * @return Padded number
+	 */
+	public static String zeroPad(int digitExpected, String numToPad) {
+		if (numToPad.length()==digitExpected) { return numToPad; }
+		String result = "";
+		while ((digitExpected---numToPad.length()) > 0) { result += "0"; }
+		return result+numToPad;
+	}
+	
+	
+	/**
+	 * Removes extra decimals.
+	 * @param decimalExpected Maximum number of decimal 
+	 * @param num Number to test decimal
+	 * @return number with <= decimalExpected
+	 */
+	public static double fixedDecimal(int decimalExpected, double num) {
+		String[] fullNum = (""+(num)).split("\\.", 2);
+		String decimal = zeroPad(decimalExpected, fullNum[1]).substring(0, decimalExpected);
+		return Double.parseDouble(((int)num)+"."+decimal);
 	}
 }
