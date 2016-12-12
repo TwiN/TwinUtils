bc4df8f3e97ab06829620e97aaa7e04e34f0daf7
TwinProduction <christian-ct@hotmail.com>
2016-12-12 18:07:09 -0500

    Added getExtension

--- src/org/twinnation/twinutilities/FileUtils.java
+++ src/org/twinnation/twinutilities/FileUtils.java
@@ -83,4 +83,14 @@ public final class FileUtils {
 		return (new File(fName).exists());
 	}
 	
+	
+	/**
+	 * Gets the extension of a file or a path
+	 * @param fileNameOrPath Name of the file or path toward the file
+	 * @return The extension of the file
+	 */
+	public static String getExtension(String fileNameOrPath) {
+		return fileNameOrPath.substring(fileNameOrPath.lastIndexOf('.')+1);
+	}
+	
 }
