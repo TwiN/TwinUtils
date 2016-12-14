be2ef29ad569724bea468cb8072ef03b0fc8e924
TwinProduction <christian-ct@hotmail.com>
2016-12-14 00:26:32 -0500

    Added mkdirIfNotExist

--- src/org/twinnation/twinutilities/FileUtils.java
+++ src/org/twinnation/twinutilities/FileUtils.java
@@ -93,6 +93,7 @@ public final class FileUtils {
 		return fileNameOrPath.substring(fileNameOrPath.lastIndexOf('.')+1);
 	}
 	
+	
 	/**
 	 * Gets the base name of a file or a path
 	 * @param fileNameOrPath Name of the file or path toward the file
@@ -108,5 +109,22 @@ public final class FileUtils {
 		}
 		return fileNameOrPath.substring(0, fileNameOrPath.lastIndexOf('.'));
 	}
+	
+	
+	/**
+	 * Creates a directory at the given path if it doesn't already exist
+	 * @param pathToDir Path to the directory (e.g. /home/twin/DIRECTORY_NAME)
+	 * @return Whether the directory was created (true) or already existed (false)
+	 */
+	public static boolean mkdirIfNotExist(String pathToDir) {
+		File dir = new File(pathToDir);
+		if (!dir.exists()) {
+			if (dir.mkdir()) {
+				return true; // directory was created 
+			}
+		}
+		return false; // directory already exists
+	}
+	
 	
 }
