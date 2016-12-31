e333e9f26418fc7e45f2b652c66d6b9870897972
TwinProduction <christian-ct@hotmail.com>
2016-12-30 20:40:33 -0500

    Added isPortBusy

--- src/org/twinnation/twinutilities/NetUtils.java
+++ src/org/twinnation/twinutilities/NetUtils.java
@@ -1,5 +1,9 @@
 package org.twinnation.twinutilities;
 
+import java.io.IOException;
+import java.net.Socket;
+import java.net.SocketException;
+
 /**
  * Network-related utility class
  */
@@ -38,5 +42,21 @@ public final class NetUtils {
 	public static boolean isValidIPv6(String ip) {
 		return ip.toUpperCase()
 				.matches("\\A(?:[0-9A-F]{1,4}:){7}[0-9A-F]{1,4}\\z");
+	}
+	
+	
+	/**
+	 * Checks if the port on a given host is busy 
+	 * @param host Target host (ip, domain name)
+	 * @param port Port to check
+	 * @return Whether the port on the given host is busy 
+	 */
+	public static boolean isPortBusy(String host, int port) {
+		try {
+			(new Socket(host, port)).close(); 
+		} catch(IOException e) {
+			return false; // Couldn't connect to host:port (port not in use)
+		}
+		return true; // Connected to host:port (port in use)
 	}
 }
