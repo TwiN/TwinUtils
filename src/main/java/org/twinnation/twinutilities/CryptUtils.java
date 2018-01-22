ee9e3564cfa990cbc820a508ef418e04b82364c3
TwinProduction <christian-ct@hotmail.com>
2018-01-22 17:30:04 -0500

    Improved class structure + updated comments

--- src/main/java/org/twinnation/twinutilities/CryptUtils.java
+++ src/main/java/org/twinnation/twinutilities/CryptUtils.java
@@ -18,34 +18,128 @@ public class CryptUtils {
 	
 	
 	/**
+	 * Hashes a String using Secure Hash Algorithm 512 bits
+	 * @param str String to hash
+	 * @return hash
+	 */
+	public static String sha512(String str) {
+		return sha(512, str);
+	}
+	
+	
+	/**
+	 * Hashes a String using Secure Hash Algorithm 256 bits
+	 * @param str String to hash
+	 * @return hash
+	 */
+	public static String sha256(String str) {
+		return sha(256, str);
+	}
+	
+	
+	/**
+	 * Hashes a String using the given version of Secure Hash Algorithm
+	 * @param bits SHA version (bits) ex: 256, 512, ...
+	 * @param str String to hash
+	 * @return hash
+	 */
+	private static String sha(int bits, String str) {
+		String result = "";
+		try {
+			MessageDigest digest = MessageDigest.getInstance("SHA-" + bits);
+			byte[] bytes = digest.digest(str.getBytes("UTF-8"));
+			result = new BigInteger(1, bytes).toString(16);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+	
+	
+	/**
+	 * Uses the SHA512 algorithm to create a salted hash for the given password 
+	 * @param str String to hash
+	 * @param salt Salt
+	 * @return hash:salt
+	 */
+	public static String sha512Salted(String str, String salt) {
+		return sha512(str) + ":" + salt;
+	}
+	
+	
+	/**
+	 * Validates a string generated with CryptUtils.sha512Salted
+	 * @param str String (not hashed)
+	 * @param hashSalt hash:salt
+	 * @return Whether the password matches or not
+	 */
+	static boolean validateSha512Salted(String str, String hashSalt) {
+		String salt = hashSalt.split(":")[1];
+		return sha512Salted(str, salt).equalsIgnoreCase(hashSalt);
+	}
+	
+	
+	/**
+	 * Generates a secure salt
+	 * @return Random salt
+	 */
+	public static String getSalt() {
+		byte[] salt = new byte[24];
+		SecureRandom sr = new SecureRandom();
+		sr.nextBytes(salt);
+		return base64encode(salt);
+	}
+	
+	
+	
+	/**
+	 * Rotates the alphabetic characters in a string by 13 positions
+	 * @param str String to encrypt with rot13
+	 * @return Encrypted String
+	 */
+	static String rot13(String str) {
+		for(int i = 0; i < str.length(); i++) {
+			char c = str.charAt(i);
+			if ((c >= 'a' && c <= 'm') || (c >= 'A' && c <= 'M')) {
+				c += 13;
+			} else if ((c >= 'n' && c <= 'z') || (c >= 'N' && c <= 'Z')) {
+				c -= 13;
+			}
+			str = str.substring(0, i) + c + str.substring(i+1);
+		}
+		return str;
+	}
+	
+	
+	/**
 	 * Encodes a byte array using base64
-	 * @param b Byte array
+	 * @param bytes Byte array
 	 * @return Base64 encoded string
 	 */
-	static String base64encode(byte[] b) {
-		return Base64.getEncoder().encodeToString(b);
+	public static String base64encode(byte[] bytes) {
+		return Base64.getEncoder().encodeToString(bytes);
 	}
 	
 	
 	/**
 	 * Encodes a String using base64
-	 * @param s String to encode
+	 * @param str String to encode
 	 * @return Base64 encoded string
 	 */
-	static String base64encode(String s) {
-		return Base64.getEncoder().encodeToString(s.getBytes());
+	public static String base64encode(String str) {
+		return Base64.getEncoder().encodeToString(str.getBytes());
 	}
 	
 	
 	/**
 	 * Decodes a base64 encoded byte array
-	 * @param b Byte array 
+	 * @param bytes Byte array
 	 * @return Decoded String
 	 */
-	static String base64decode(byte[] b) {
+	public static String base64decode(byte[] bytes) {
 		String decodedString = "";
 		try {
-			decodedString = new String(Base64.getDecoder().decode(b), "UTF-8");
+			decodedString = new String(Base64.getDecoder().decode(bytes), "UTF-8");
 		} catch (UnsupportedEncodingException e) {
 			e.printStackTrace();
 		}
@@ -55,94 +149,17 @@ public class CryptUtils {
 	
 	/**
 	 * Decodes a base64 encoded String
-	 * @param s Base64 encoded String
+	 * @param str Base64 encoded String
 	 * @return Decoded String
 	 */
-	static String base64decode(String s) {
+	public static String base64decode(String str) {
 		String decodedString = "";
 		try {
-			decodedString = new String(Base64.getDecoder().decode(s.getBytes()), "UTF-8");
+			decodedString = new String(Base64.getDecoder().decode(str.getBytes()), "UTF-8");
 		} catch (UnsupportedEncodingException e) {
 			e.printStackTrace();
 		}
 		return decodedString;
-	}
-	
-	
-	static String sha(int bits, String s) {
-		String result = "";
-		try {
-			MessageDigest digest = MessageDigest.getInstance("SHA-" + bits);
-			byte[] passwordBytesArray = digest.digest(s.getBytes("UTF-8"));
-			result = new BigInteger(1, passwordBytesArray).toString(16);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return result;
-	}
-	
-	
-	static String sha512(String s) {
-		return sha(512, s);
-	}
-	
-	
-	static String sha256(String s) {
-		return sha(256, s);
-	}
-	
-	
-	/**
-	 * Uses the SHA512 algorithm to create a salted hash for the given password 
-	 * @param password Password
-	 * @param salt Salt
-	 * @return PasswordHash:PasswordSalt
-	 */
-	static String sha512Salted(String password, String salt) {
-		return sha512(password) + ":" + salt;
-	}
-	
-	
-	/**
-	 * Generates a secure salt
-	 * @return Random salt
-	 */
-	static String getSalt() {
-		byte[] salt = new byte[24];
-		SecureRandom sr = new SecureRandom();
-		sr.nextBytes(salt);
-		return base64encode(salt);
-	}
-	
-	
-	/**
-	 * Validates a password generated with CryptUtils.sha512Salted
-	 * @param password Password
-	 * @param hashSalt PasswordHash:PasswordSalt
-	 * @return Whether the password matches or not
-	 */
-	static boolean validateSha512Salted(String password, String hashSalt) {
-		String salt = hashSalt.split(":")[1];
-		return sha512Salted(password, salt).equalsIgnoreCase(hashSalt);
-	}
-	
-	
-	/**
-	 * Rotates the alphabetic characters in a string by 13 positions
-	 * @param s String to encrypt with rot13
-	 * @return Encrypted String
-	 */
-	static String rot13(String s) {
-		for(int i = 0; i < s.length(); i++) {
-			char c = s.charAt(i);
-			if ((c >= 'a' && c <= 'm') || (c >= 'A' && c <= 'M')) {
-				c += 13;
-			} else if ((c >= 'n' && c <= 'z') || (c >= 'N' && c <= 'Z')) {
-				c -= 13;
-			}
-			s = s.substring(0, i) + c + s.substring(i+1);
-		}
-		return s;
 	}
 	
 }
